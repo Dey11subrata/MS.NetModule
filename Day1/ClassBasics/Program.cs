c566f38f9c90b76795613632902c298d329815d1
subrata <deysubrata2311@gmail.com>
2023-01-10 03:20:04 +0530

    day_01 repo created

---
+++ Day1/ClassBasics/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace ClassBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("hello world");
+        }
+    }
+
+    public class Class1 { 
+    }
+
+}
